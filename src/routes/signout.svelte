1ec6505795475fee8f6c8dc1ae3e79dc34c805b4
Akash <akash2003m@gmail.com>
2022-02-07 22:54:05 +0530

    Signout SCSS

--- src/routes/signout.svelte
+++ src/routes/signout.svelte
@@ -16,12 +16,35 @@
 <div>
 	<Header {page} />
 	<div>
-		<center>
-			<h2>Are you sure to signout?</h2>
-			<button on:click|preventDefault={() => ($session === null ? goto('/auth') : goto('/todo'))}
-				>No</button
-			>
-			<button on:click|preventDefault={logOut}>Yes</button>
-		</center>
+		<div class="container">
+			<center>
+				<div class="card">
+					<h2>Are you sure to signout?</h2>
+					<button
+						on:click|preventDefault={() => ($session === null ? goto('/auth') : goto('/todo'))}
+						>No</button
+					>
+					<button on:click|preventDefault={logOut}>Yes</button>
+				</div>
+			</center>
+		</div>
 	</div>
 </div>
+
+<style lang="scss">
+	.container {
+		min-height: 90vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: #eee;
+	}
+	.container .card {
+		height: 120px;
+		width: 300px;
+		background-color: #fff;
+		position: relative;
+		box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
+		padding-top: 10px;
+	}
+</style>
