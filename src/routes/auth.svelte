7e6977a29f188c1105c1eb5ff631ac3f05562e09
Akash <akash2003m@gmail.com>
2022-02-08 15:28:13 +0530

    Extensive SCSS

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -39,56 +39,42 @@
 <main class="auth">
 	<Header {page} />
 
-	<div class="container">
-		<center>
-			<div class="card">
-				<h1>SignUp</h1>
-				<table>
-					<tr>
-						<td>Enter mail-id :</td>
-						<td><input type="email" bind:value={mail} /></td>
-					</tr>
-					<tr>
-						<td>Enter password : </td>
-						<td><input type="password" bind:value={pass} /></td>
-					</tr>
-					<tr>
-						<td colspan="2"
-							><center
-								><div class="but-div">
-									<button on:click|preventDefault={connect}>Submit</button>
-								</div></center
-							></td
-						>
-					</tr>
-				</table>
-				<small>*Already an user? SignIn</small>
+	<div class="center">
+		<div class="property-card">
+			<div class="property-image">
+				<div class="property-image-title">
+					<center>
+						<table>
+							<tr>
+								<td>Enter mail-id :</td>
+								<td><input type="email" bind:value={mail} /></td>
+							</tr>
+							<tr>
+								<td>Enter password:</td>
+								<td><input type="password" bind:value={pass} /></td>
+							</tr>
+							<tr>
+								<td colspan="2"
+									><center
+										><div class="but-div">
+											<button on:click|preventDefault={connect}>Submit</button>
+										</div></center
+									></td
+								>
+							</tr>
+						</table>
+						<small>*Already an user ? SignIn</small>
+					</center>
+				</div>
 			</div>
-		</center>
+			<div class="property-description">
+				<h5>SignUp</h5>
+			</div>
+		</div>
 	</div>
-	<!--<pre>{JSON.stringify($session, null, 2)}</pre>-->
 </main>
 
 <style lang="scss">
-	.container {
-		min-height: 90vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-color: #eee;
-	}
-	.container .card {
-		height: 280px;
-		width: 500px;
-		background-color: #fff;
-		position: relative;
-		box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
-		padding-top: 10px;
-	}
-	input {
-		font: inherit;
-		padding: 5px 12px;
-	}
 	table,
 	tr,
 	td {
@@ -121,18 +107,64 @@
 	button:focus {
 		outline: none;
 	}
-	@media (max-width: 700px) {
-		.container {
-			overflow: hidden;
-			min-height: 95vh;
-			.card{
-				width: 310px;
-				height: 300px;
-			}
-		}
-		input{
-			size: 10;
-			padding: 1px 1px 1px 1px;
-		}
+
+	* {
+		box-sizing: border-box;
+	}
+
+	h5 {
+		margin: 0px;
+		font-size: 1.4em;
+		font-weight: 700;
+	}
+	.center {
+		height: 90vh;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: grey;
+	}
+	.property-card {
+		height: 15em;
+		width: 20em;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		border-radius: 16px;
+		overflow: hidden;
+		box-shadow: 15px 15px 27px grey, -15px -15px 27px grey;
+	}
+	.property-image {
+		height: 30em;
+		width: 20em;
+		padding: 1em 2em;
+		position: absolute;
+		top: 0px;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		background-image: linear-gradient(rgba(240, 0, 0, 0.6), rgba(245, 0, 230, 0.4));
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+	.property-description {
+		background-color: #fafafc;
+		height: 10em;
+		width: 20em;
+		position: absolute;
+		bottom: 0em;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		padding: 0.5em 1em;
+		text-align: center;
+	}
+	.property-card:hover {
+		box-shadow: 15px 15px 27px #121212, -15px -15px 27px grey;
+	}
+	.property-card:hover .property-description {
+		height: 0em;
+		padding: 0px 1em;
+	}
+	.property-card:hover .property-image {
+		height: 15em;
 	}
 </style>
--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -27,49 +27,42 @@
 
 <main>
 	<Header {page} />
-	<div class="container">
-		<div class="card">
-			<center>
-				<h1>SignIn</h1>
-				<table>
-					<tr>
-						<td>Enter mail-id :</td>
-						<td><input type="email" bind:value={mail} /></td>
-					</tr>
-					<tr>
-						<td>Enter password:</td>
-						<td><input type="password" bind:value={pass} /></td>
-					</tr>
-					<tr>
-						<td colspan="2"><center><div class='but-div'><button on:click|preventDefault={connect}>Submit</button></div></center></td>
-					</tr>
-				</table>
-				<small>*New user SignUp</small>
-			</center>
+	<div class="center">
+		<div class="property-card">
+			<div class="property-image">
+				<div class="property-image-title">
+					<center>
+						<table>
+							<tr>
+								<td>Enter mail-id :</td>
+								<td><input type="email" bind:value={mail} /></td>
+							</tr>
+							<tr>
+								<td>Enter password:</td>
+								<td><input type="password" bind:value={pass} /></td>
+							</tr>
+							<tr>
+								<td colspan="2"
+									><center
+										><div class="but-div">
+											<button on:click|preventDefault={connect}>Submit</button>
+										</div></center
+									></td
+								>
+							</tr>
+						</table>
+						<small>*New user SignUp</small>
+					</center>
+				</div>
+			</div>
+			<div class="property-description">
+				<h5>SignIn</h5>
+			</div>
 		</div>
 	</div>
 </main>
 
 <style lang="scss">
-	.container {
-		min-height: 90vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-color: #eee;
-	}
-	.container .card {
-		height: 280px;
-		width: 500px;
-		background-color: #fff;
-		position: relative;
-		box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1);
-		padding-top: 10px;
-	}
-	input {
-		font: inherit;
-		padding: 5px 12px;
-	}
 	table,
 	tr,
 	td {
@@ -80,7 +73,7 @@
 	small {
 		color: red;
 	}
-	.but-div{
+	.but-div {
 		padding: 10px;
 	}
 	button {
@@ -102,18 +95,63 @@
 	button:focus {
 		outline: none;
 	}
-	@media (max-width: 700px) {
-		.container {
-			overflow: hidden;
-			min-height: 95vh;
-			.card{
-				width: 310px;
-				height: 300px;
-			}
-		}
-		input{
-			size: 10;
-			padding: 1px 1px 1px 1px;
-		}
+	* {
+		box-sizing: border-box;
+	}
+
+	h5 {
+		margin: 0px;
+		font-size: 1.4em;
+		font-weight: 700;
+	}
+	.center {
+		height: 90vh;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: grey;
+	}
+	.property-card {
+		height: 15em;
+		width: 20em;
+		display: flex;
+		flex-direction: column;
+		position: relative;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		border-radius: 16px;
+		overflow: hidden;
+		box-shadow: 15px 15px 27px grey, -15px -15px 27px grey;
+	}
+	.property-image {
+		height: 30em;
+		width: 20em;
+		padding: 1em 2em;
+		position: absolute;
+		top: 0px;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		background-image: linear-gradient(rgba(240, 0, 200, 0.6), rgba(245, 240, 100, 0.6));
+		background-size: cover;
+		background-repeat: no-repeat;
+	}
+	.property-description {
+		background-color: #fafafc;
+		height: 10em;
+		width: 20em;
+		position: absolute;
+		bottom: 0em;
+		transition: all 0.4s cubic-bezier(0.645, 0.045, 0.355, 1);
+		padding: 0.5em 1em;
+		text-align: center;
+	}
+	.property-card:hover {
+		box-shadow: 15px 15px 27px #121212, -15px -15px 27px grey;
+	}
+	.property-card:hover .property-description {
+		height: 0em;
+		padding: 0px 1em;
+	}
+	.property-card:hover .property-image {
+		height: 15em;
 	}
 </style>
