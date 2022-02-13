4d61d22199fbe71da66d19379f5b02d5f122a292
Akash <akash2003m@gmail.com>
2022-02-14 00:04:30 +0530

    Home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,26 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import Animation from '$lib/animation.svelte';
 	const page = 'home';
 </script>
 
-<main class='home'>
-	<Header {page} />
+<div class="home">
+	<div>
+		<Header {page} />
+	</div>
+	<div class="home-div">
+		<div>
+			<Animation />
+		</div>
+		<div />
+	</div>
+</div>
 
-	<p>Home</p>
-</main>
-
-
-<style>
-	p{
-		padding: 10px;
-		font-family: 'Open Sans', sans-serif;
-		font-size: 14px;
+<style lang="scss">
+	.home {
+		background-color: #222121;
+		height: 100vh;
+		width: 100%;
+		color: #fff;
 	}
 </style>
